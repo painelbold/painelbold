db1e8f14a53a1b95672c02c32f81d6e0d334d0c1
App Painel Bold <app.painelbold@gmail.com>
2018-05-11 22:52:58 -0300

    Adicionadas verificações no form de registro. Adicionados arquivos para modelos

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,7 @@
 import { ListPage } from './../list/list';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'page-register',
@@ -8,21 +9,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
   selectedItem: any;
+  masks: any;
+
   tipoRegistro: string;
   tipoCond: string;
+
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+
+  items: Array<{ title: string, note: string, icon: string }>;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.tipoRegistro = 'u';
     this.tipoCond = 'a';
+    
+    
   }
 
-  goBack(event){
+  goBack(event) {
     this.navCtrl.pop();
-  } 
+  }
 
-  registrar(event){
-    this.navCtrl.setRoot(ListPage)
+  onRegister(form: NgForm) {
+    console.log(form);
+    this.navCtrl.setRoot(ListPage);
   }
 }
