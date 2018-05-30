22b2fc7608e9efb1d5990104322110bdb58cdbae
App Painel Bold <app.painelbold@gmail.com>
2018-05-30 05:51:57 -0300

    Métodos básicos do CRUD para comunicados

--- src/models/announcement.ts
+++ src/models/announcement.ts
@@ -1,6 +1,7 @@
 import { User } from './user';
 export class Announcement {
     constructor(
+        public key: string,
         public title: string, 
         public message: string, 
         public publishDate: Date,  
--- src/providers/announcement/announcement.ts
+++ src/providers/announcement/announcement.ts
@@ -1,17 +1,47 @@
-import { HttpClient } from '@angular/common/http';
+import { Announcement } from './../../models/announcement';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the AnnouncementProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AnnouncementProvider {
+  private PATH='announcements/';
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AnnouncementProvider Provider');
+  constructor(private db: AngularFireDatabase) {
   }
 
+  getAll(){
+    return this.db.list(this.PATH)
+    .snapshotChanges()
+    .map(changes=>{
+      return changes.map(c=>({ key: c.payload.key, ...c.payload.val() }))
+    });
+  }
+  
+  get(key: string){
+    return this.db.object(this.PATH + key)
+    .snapshotChanges()
+    .map(c => {
+      return { key: c.key, ...c.payload.val() };
+    })
+
+  }
+
+  save(announcement: Announcement){
+    return new Promise((resolve, reject) => {
+      if(announcement.key){
+        this.db.list(this.PATH)
+        .update(announcement.key, announcement )
+        .then(() =>resolve())
+        .catch((e) => reject(e))
+      } else {
+        this.db.list(this.PATH)
+        .push( announcement )
+        .then((result: any) =>resolve(result.key))
+      }
+    });
+  }
+
+  remove(key: string){
+    return this.db.list(this.PATH).remove(key);
+  }
 }
